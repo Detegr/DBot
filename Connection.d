d0bb9339898f3347112a95f645f326af8a67fb7f
Detegr <detegr@gmail.com>
2012-02-16 12:30:43 +0200

    Simple connection working

--- Connection.d
+++ Connection.d
@@ -1,37 +1,75 @@
 import std.stdio;
 import std.socket;
 
+class Irc
+{
+	static string User(Connection c)
+	{
+		return Message("USER " ~ c.realname ~ " " ~ c.realname ~ " * :" ~ c.realname);
+	}
+	static string Nick(Connection c)
+	{
+		return Message("NICK " ~ c.nick);
+	}
+	static string Message(string msg)
+	{
+		return (msg ~ "\r\n");
+	}
+}
+
 class Connection
 {
-	string nick;
-	string realname;
-	TcpSocket socket;
-	Address[] address;
+	private:
+		immutable auto BUFSIZE=1024;
+		string nick;
+		string realname;
+		TcpSocket socket;
+		Address[] address;
 
-	this(string nick="DBot", string realname="DBot")
-	{
-		this.nick=nick;
-		this.realname=realname;
-		socket = new TcpSocket(AddressFamily.INET);
-	}
-	~this()
-	{
-		Disconnect();
-	}
-	void Connect(scope InternetAddress addr)
-	{
-		writeln("Connecting to " ~ addr.toHostNameString() ~ ":" ~ addr.toPortString());
-		socket.connect(addr);
-	}
-	void Disconnect()
-	{
-		socket.shutdown(SocketShutdown.BOTH);
-		socket.close();
-	}
+	public:
+		this(string nick="DBot", string realname="DBot")
+		{
+			this.nick=nick;
+			this.realname=realname;
+			socket = new TcpSocket(AddressFamily.INET);
+		}
+		~this()
+		{
+			Disconnect();
+		}
+		void Connect(scope InternetAddress addr)
+		{
+			writeln("Connecting to " ~ addr.toHostNameString() ~ ":" ~ addr.toPortString());
+			socket.connect(addr);
+			Send(Irc.Nick(this));
+			Send(Irc.User(this));
+		}
+		void Send(const string msg)
+		{
+			socket.send(msg);
+		}
+		void Recv()
+		{
+			ptrdiff_t recvd;
+			char[BUFSIZE] buf = new char[BUFSIZE];
+			do
+			{
+				recvd=socket.receive(buf); // socket.receive() will check buf's bounds.
+				buf[recvd-2]=0; // Slice out \r\n
+				writeln(buf);
+			}
+			while(recvd>0);
+		}
+		void Disconnect()
+		{
+			socket.shutdown(SocketShutdown.BOTH);
+			socket.close();
+		}
 }
 
 void main()
 {
 	scope auto c = new Connection();
 	c.Connect(new InternetAddress("irc.quakenet.org", 6667));
+	c.Recv();
 }
